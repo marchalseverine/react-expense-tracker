e7155a1f46545ee3fa81d3bd3f791c829df3ba88
marchalseverine <81558460+marchalseverine@users.noreply.github.com>
2021-10-14 13:46:49 +0200

    Work with useState

--- src/components/ExpensesItem.js
+++ src/components/ExpensesItem.js
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import ExpensesDate from "./ExpensesDate";
 import "./ExpensesItem.css";
 import Card from "./Card";
 
 export default function ExpensesItem(props) {
+  const [title, setTitle] = useState(props.title);
+
+  const handleClick = (event) => {
+    event.preventDefault();
+    setTitle("Update!!");
+  };
+
   return (
     <Card>
       <div className="expense-item">
@@ -13,7 +20,7 @@ export default function ExpensesItem(props) {
               <ExpensesDate date={props.date} />
             </div>
             <div className="col-3 expense-item___description">
-              <span>{props.title}</span>
+              <span>{title}</span>
             </div>
             <div className="col-3 expense-item___amount">
               <span>{props.amount}€</span>
@@ -21,6 +28,8 @@ export default function ExpensesItem(props) {
           </div>
         </div>
       </div>
+
+      <button onClick={handleClick}>Change title</button>
     </Card>
   );
 }
