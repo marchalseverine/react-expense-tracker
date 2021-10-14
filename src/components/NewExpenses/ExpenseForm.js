8be1a3e110e1f51a073a8bd825a1a74df87f65fc
marchalseverine <81558460+marchalseverine@users.noreply.github.com>
2021-10-14 15:52:17 +0200

    Update Expensesform.js : useState

--- src/components/NewExpenses/ExpenseForm.js
+++ src/components/NewExpenses/ExpenseForm.js
@@ -1,26 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 import "./NewExpenses.css";
 
 export default function ExpenseForm() {
+  const [title, setTitle] = useState();
+  const [date, setDate] = useState();
+  const [amount, setAmount] = useState();
+
+  const handleTitleChange = (event) => {
+    setTitle(event.target.value);
+  };
+
+  const handleAmountChange = (event) => {
+    setAmount(event.target.value);
+  };
+
+  const handleDateChange = (event) => {
+    setDate(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const expenseData = {
+      title: title,
+      date: date,
+      amount: amount,
+    };
+    console.log(expenseData);
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div className="container expense-form">
         <div className="row">
           <div className="col-4">
             Date
             <div className="expense___control___date">
-              <input type="date" />
+              <input type="date" onChange={handleDateChange} />
             </div>
           </div>
           <div className="col-4">
             Title
             <div className="expense___control___title">
-              <input type="text" placeholder="What?" />
+              <input
+                type="text"
+                placeholder="What?"
+                onChange={handleTitleChange}
+              />
             </div>
           </div>
           <div className="col-4">
             Amount
-            <div className="expense___control___amount">
+            <div
+              className="expense___control___amount"
+              onChange={handleAmountChange}
+            >
               <input type="number" min="0.00" step="0.00" placeholder="€" />
             </div>
           </div>
